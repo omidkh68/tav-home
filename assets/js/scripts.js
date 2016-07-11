293ad9b474553af36ea9dc8ba107050bcd4c1c8e
omidkh68 <omidkh68@gmail.com>
2016-07-11 15:00:32 +0330

    1. rightmenu links 2. add tabular menu design 3. add language container 4. add nav-bottom items links 5. add sticky top nav header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,5 +2,20 @@
  * Created by omi001kho on 05/07/2016.
  */
 $(document).ready(function() {
+    var $body = $('body'),
+        $topNav = $('.nav-top'),
+        $header = $('header');
+
     $('[data-toggle="tooltip"]').tooltip();
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 80){
+            $topNav.addClass("sticky");
+            $header.addClass('sticky-header');
+        }
+        else if($(this).scrollTop() < 20){
+            $topNav.removeClass("sticky");
+            $header.removeClass('sticky-header');
+        }
+    });
+
 });
