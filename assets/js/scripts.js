b068f00cfb98e91e9c69c809cedb506c0796950c
omidkh68 <omidkh68@gmail.com>
2016-07-12 14:44:22 +0330

    1. add owl-carousel 2. add new-ticker 3. add footer 4. add copyright 5. add following website in rightmenu 6. add weather widget 7. add goToTop button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,9 +4,37 @@
 $(document).ready(function() {
     var $body = $('body'),
         $topNav = $('.nav-top'),
-        $header = $('header');
+        $header = $('header'),
+        $goToTop = $('#goToTop');
 
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('.owl-carousel').owlCarousel({
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        items:1,
+        margin:0,
+        loop:true,
+        nav:true,
+        autoHeight: true,
+        autoplay: false
+    });
+
+    $('.typing').inewsticker({
+        speed: 7000,
+        effect: 'fade',
+        dir: 'rtl',
+        delay_after: 4000
+    });
+
+    $goToTop.on('click',function(e) {
+        e.preventDefault();
+
+        $body.animate({
+            scrollTop: 0
+        }, 300);
+    });
+
     $(window).scroll(function() {
         if($(this).scrollTop() > 80){
             $topNav.addClass("sticky");
@@ -15,6 +43,14 @@ $(document).ready(function() {
         else if($(this).scrollTop() < 20){
             $topNav.removeClass("sticky");
             $header.removeClass('sticky-header');
+        }
+
+        if($(this).scrollTop() > 200){
+            $goToTop.addClass('active');
+        }
+        else
+        {
+            $goToTop.removeClass('active');
         }
     });
 
